a219da1e4d15f90903336d357ff71115c58af559
Matt Massicotte <85322+mattmassicotte@users.noreply.github.com>
2019-02-02 21:05:48 -0500

    OperationQueue enqueuing helper
    - OperationQueue.addOperations with a default to not wait for their completion
    - OperationExpectation can now wait for arrays of operations

---
+++ OperationPlus/Extensions/OperationQueue+Enqueuing.swift
@@ -0,0 +1,21 @@
+//
+//  OperationQueue+Enqueuing.swift
+//  OperationPlus
+//
+//  Created by Matt Massicotte on 2019-02-02.
+//  Copyright © 2019 Chime Systems Inc. All rights reserved.
+//
+
+import Foundation
+
+extension OperationQueue {
+
+    /// Adds the specified operations to the queue.
+    ///
+    /// This method is just a wrapper for calling `addOperations(ops, waitUntilFinished: false)`
+    ///
+    /// - Parameter ops: The operations to be added to the queue.
+    public func addOperations(_ ops: [Operation]) {
+        addOperations(ops, waitUntilFinished: false)
+    }
+}
